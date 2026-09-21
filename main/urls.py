f5bdefb101c3b75bc385fb852c99a52817d86405
Fadlan Fathul Islam <fadlanfathul@gmail.com>
2026-09-21 21:59:04 +0700

    feat(Individual Assignment 3): Menambahkan URL untuk Fungsi yang Baru Dibuat

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_education, create_education, get_education_json, delete_education
+from main.views import *
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
+    path("experience/add/", create_experience, name="create_experience"),
+    path("experience/<uuid:experience_id>/update/", update_experience, name="update_experience"),
+    path("experience/<uuid:experience_id>/delete/", delete_experience, name="delete_experience"),
+    path("api/experience/", get_experience_json, name="get_experience_json"),
     path("education/", show_education, name="show_education"),
     path("education/add/", create_education, name="create_education"),
     path("api/education/", get_education_json, name="get_education_json"),
